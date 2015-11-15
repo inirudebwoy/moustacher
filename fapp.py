d0d3b119a62580d8acba6b7e7ecb73b985737359
Michael Klich <michal@michalklich.com>
2015-11-15 17:33:32 +0000

    List templates in dir

--- fapp.py
+++ fapp.py
@@ -1,6 +1,30 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+TMPL_DIR = 'templates'
+
+
+def _get_templates():
+    """ List templates in the dir """
+    try:
+        return os.listdir(TMPL_DIR)
+    except OSError:
+        return []
+
+
+@app.route('/templates')
+def template_list():
+    """ Return list of templates """
+    return jsonify({'templates': _get_templates()})
+
+
+@app.route('/compose', methods=['POST'])
+def compose():
+    """ Compose image with template """
+    return jsonify({'image': ''})
 
 
 if __name__ == '__main__':
