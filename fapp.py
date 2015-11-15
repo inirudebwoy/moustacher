004d3406c97cf02157c353319cd0c1f83c554bca
Michael Klich <michal@michalklich.com>
2015-11-15 21:20:44 +0000

    Add jinja, prepare template generation

--- fapp.py
+++ fapp.py
@@ -1,11 +1,13 @@
 import os
 import os.path
+import subprocess
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 TMPL_DIR = 'templates'
+RASTER = 'rasterize.js'
 
 
 def _get_templates():
@@ -25,10 +27,33 @@ def template_list():
     return jsonify({'templates': _get_templates()})
 
 
+def _compose(img, template):
+    # update jinja template, insert img to a tag
+    return template
+
+
+def _generate_img(template):
+    output_img = None
+    pipe = subprocess.Popen(
+        ['phantomjs', '/dev/stdin', template, output_img],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    stdout, stderr = pipe.communicate(input=RASTER)
+    if pipe.returncode != 0:
+        raise RuntimeError(
+            "PhantomJS didn't execute successfully - STDOUT: %s STDERR: %s" % (
+                stdout, stderr))
+    return output_img
+
+
 @app.route('/compose', methods=['POST'])
 def compose():
     """ Compose image with template """
-    return jsonify({'image': ''})
+    img = request.json['img']
+    template = request.json['template']
+    # make a template
+    new_template = _compose(img, template)
+    return jsonify({'image': _generate_img(new_template)})
 
 
 if __name__ == '__main__':
