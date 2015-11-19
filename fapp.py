d75192cb454273c3b1d579cc8f3ffb716cce15ef
Michael Klich <michal@michalklich.com>
2015-11-19 21:59:27 +0000

    Add skeleton

--- fapp.py
+++ fapp.py
@@ -1,17 +1,33 @@
 import os
 import os.path
+import shutil
 import subprocess
+import tempfile
 
 from flask import Flask, jsonify, request
+from jinja2 import Template
 
 app = Flask(__name__)
 
 TMPL_DIR = 'templates'
-RASTER = 'rasterize.js'
+RASTER = 'output.js'
+TMPL_INDEX = 'index.html'
+CURRENT_PATH = os.path.abspath('.')
+IMG_TAG_SRC = 'data:image/png;base64,{image}'
 
 
 def _get_templates():
-    """ List templates in the dir """
+    """
+    List templates in the dir
+
+    Each template in dir:
+    - should be in a separate directory which name will be used as a template
+      name.
+    - should have index.html file within the root of it's dir.
+    - should provide all the assets needed
+    - within index.html there should be a template tag {{ dick_pic }}
+
+    """
     try:
         return os.listdir(TMPL_DIR)
     except OSError:
@@ -27,33 +43,47 @@ def template_list():
     return jsonify({'templates': _get_templates()})
 
 
-def _compose(img, template):
-    # update jinja template, insert img to a tag
-    return template
+def _compose(img, template_name):
+    tmpl_path = os.path.join(CURRENT_PATH, TMPL_DIR, template_name)
+    # create temp files in template dir
+    html = tempfile.NamedTemporaryFile(dir=tmpl_path)
+    js = tempfile.NamedTemporaryFile(dir=tmpl_path)
+
+    # copy contents of real files into temp files
+    html.file.write(open(os.path.join(tmpl_path, TMPL_INDEX)).read())
+    js.file.write(open(os.path.join(CURRENT_PATH, RASTER)).read())
+
+    # update index.html.tmp, {{ dick_pic }}
+    template_html = Template(html.read())
+    img_tag = IMG_TAG_SRC.format(image=img)
+    html.write(template_html.render(dick_pic=img_tag))
+
+    # update output.js.tmp, {{ template_name }}
+    template_js = Template(js.read())
+    js.write(template_js.render(template_name=os.path.join(tmpl_path,
+                                                           TMPL_INDEX)))
+
+    # return temp files
+    return html.name, js.name
 
 
-def _generate_img(template):
-    output_img = None
-    pipe = subprocess.Popen(
-        ['phantomjs', '/dev/stdin', template, output_img],
-        stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-    stdout, stderr = pipe.communicate(input=RASTER)
-    if pipe.returncode != 0:
-        raise RuntimeError(
-            "PhantomJS didn't execute successfully - STDOUT: %s STDERR: %s" % (
-                stdout, stderr))
-    return output_img
+def _generate_img(template, img):
+    # make a template
+    tmp_html, tmp_js = _compose(img, template)
+    subprocess.Popen(['phantomjs', tmp_js],
+                     stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                     stderr=subprocess.STDOUT)
+    # close tmp files
+    tmp_html.close()
+    tmp_js.close()
 
 
 @app.route('/compose', methods=['POST'])
 def compose():
     """ Compose image with template """
-    img = request.json['img']
-    template = request.json['template']
-    # make a template
-    new_template = _compose(img, template)
-    return jsonify({'image': _generate_img(new_template)})
+    img = request.form['img']
+    template = request.form['template']
+    return jsonify({'image': _generate_img(template, img)})
 
 
 if __name__ == '__main__':
---
+++ output.js
@@ -0,0 +1,5 @@
+var page = require('webpage').create();
+page.open('file:///{{template_path}}', function() {
+  page.render('output.png');
+  phantom.exit();
+});
