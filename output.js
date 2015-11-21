5c77b5ece5c8adef19cda1a228a5fa75ed7ece28
Flyte <flyte@failcode.co.uk>
2015-11-21 21:11:40 +0000

    Call the jpeg the right thing

--- output.js
+++ output.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
 page.open('file://{{template_path}}', function() {
-  page.render('output.png', {format: 'jpeg', quality: '90'});
+  page.render('output.jpg', {format: 'jpeg', quality: '90'});
   phantom.exit();
 });
